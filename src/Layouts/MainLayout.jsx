b118439eec1e152219b1d90e5a42d6e643d6a21f
alimran-dev <alimran5air@gmail.com>
2023-09-21 21:19:14 +0600

    home completed

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Header />
+            <div className="flex-grow mx-20 my-10">
+            <Outlet/>
+            </div>
             <Footer/>
         </div>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <div>
-            <p>Copyright &copy; 2023 - All right reserved by Amajhon Ltd</p>
+        <div className="m-6">
+            <p className="text-white text-center py-5 rounded-md bg-zinc-900">Copyright &copy; 2023 - All right reserved by Amajhon Ltd</p>
         </div>
     );
 };
